e7f06bb2ac278bb6fe46c5b1e0bb4e7b6cd03f29
nicholas <nnnnicholas@users.noreply.github.com>
2022-07-08 17:52:28 -0400

    solmate, some contract progress

--- packages/hardhat/contracts/NFTAuctionMachine.sol
+++ packages/hardhat/contracts/NFTAuctionMachine.sol
@@ -1,32 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol"; // Only used to get opensea customization access
-
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@rari-capital/solmate/src/tokens/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "./WETH9.sol";
 import "@jbx-protocol/contracts-v2/contracts/JBETHERC20ProjectPayer.sol";
 
-contract NFTAuctionMachine is
-    ERC721,
-    ERC721Enumerable,
-    ERC721URIStorage,
-    JBETHERC20ProjectPayer,
-    Ownable
-{
-    using Counters for Counters.Counter;
+contract NFTAuctionMachine is ERC721, JBETHERC20ProjectPayer {
+    using Strings for uint256;
 
-    Counters.Counter private _tokenIdCounter;
-
+    uint256 public totalSupply = 0;
     uint256 public auctionDuration;
 
     WETH9 public weth;
 
     IJBProjectPayer public jb;
-    // IJBPaymentTerminal public terminal = directory.primaryTerminalOf(_projectId, _token);
+    // IJBPaymentTerminal public terminal;
 
     string public hardcodedTokenURI;
 
@@ -60,6 +50,7 @@ contract NFTAuctionMachine is
         auctionEndingAt = block.timestamp + auctionDuration;
         hardcodedTokenURI = uri;
         jbProjectId = projectId;
+        // terminal = directory.primaryTerminalOf(_projectId, _token); // for ProjectPayer
     }
 
     function _baseURI() internal pure override returns (string memory) {
@@ -113,8 +104,10 @@ contract NFTAuctionMachine is
         auctionEndingAt = block.timestamp + auctionDuration;
 
         if (highestBidder == address(0)) {
-            _tokenIdCounter.increment();
-            uint256 tokenId = _tokenIdCounter.current();
+            unchecked {
+                _tokenIdCounter++;
+            }
+            uint256 tokenId = _tokenIdCounter;
             _mint(address(0x000000000000000000000000000000000000dEaD), tokenId);
             _setTokenURI(tokenId, hardcodedTokenURI);
             return tokenId;
@@ -137,12 +130,52 @@ contract NFTAuctionMachine is
                 "" //bytes calldata _metadata
             );
 
-            _tokenIdCounter.increment();
-            uint256 tokenId = _tokenIdCounter.current();
+            unchecked {
+                _tokenIdCounter++;
+            }
+            uint256 tokenId = _tokenIdCounter;
             _mint(lastBidder, tokenId);
             _setTokenURI(tokenId, hardcodedTokenURI); // BYO METADATA
             return tokenId;
         }
+    }
+
+    // METADATA
+    string ipfs;
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(tokenId <= totalSupply, "Token does not exist");
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+
+        // If there is no base URI, return the token URI.
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+        // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+
+        return super.tokenURI(tokenId);
+    }
+
+    function _setTokenURI(string memory newTokenURI)
+        internal
+        virtual
+        onlyOwner
+    {
+        ipfs = newTokenURI;
+    }
+
+    function _burn(uint256 tokenId) internal virtual override {
+        super._burn(tokenId);
     }
 
     // function updateJBPaymentTerminal() external {
@@ -155,32 +188,19 @@ contract NFTAuctionMachine is
         address from,
         address to,
         uint256 tokenId
-    ) internal override(ERC721, ERC721Enumerable) {
+    ) internal override(ERC721) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceID)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceID);
-    }
+    // function supportsInterface(bytes4 interfaceID)
+    //     public
+    //     view
+    //     override(ERC721)
+    //     returns (bool)
+    // {
+    //     return
+    //         type(IERC721Enumerable).interfaceId ||
+    //         _interfaceId == type(IJBProjectPayer).interfaceId ||
+    //         super.supportsInterface(_interfaceId);
+    // }
 }
