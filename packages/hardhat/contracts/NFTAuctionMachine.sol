9e05e2e8a5251e6e9c2a2b5281a12a0fa850cc37
nicholas <nnnnicholas@users.noreply.github.com>
2022-07-08 16:57:41 -0400

    fixed imports

--- packages/hardhat/contracts/NFTAuctionMachine.sol
+++ packages/hardhat/contracts/NFTAuctionMachine.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/access/Ownable.sol"; // Only used to get opensea customization access
+import "@openzeppelin/contracts/access/Ownable.sol"; // Only used to get opensea customization access
 
 import "./WETH9.sol";
 import "@jbx-protocol/contracts-v2/contracts/JBETHERC20ProjectPayer.sol";
