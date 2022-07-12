c87fa5c95cfaa607780e9a90bcb569864b1fd9a1
viraj124 <virajm72@gmail.com>
2022-07-13 03:59:35 +0530

    chore: update interface

--- packages/hardhat/contracts/interfaces/IMetadata.sol
+++ packages/hardhat/contracts/interfaces/IMetadata.sol
@@ -3,6 +3,4 @@ pragma solidity ^0.8.6;
 
 interface IMetadata {
     function tokenURI(uint256 _tokenId) external view returns (string memory);
-
-    function isMetaDataFrozen() external view returns (bool);
 }
